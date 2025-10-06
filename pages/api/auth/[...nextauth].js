c0f3e4364f0878c99b6835c539f07d95807b57d2
raphhman-cmd <raph.hman@gmail.com>
2025-10-06 14:11:57 +0200

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -29,3 +29,15 @@ export const authOptions = {
 }
 
 export default NextAuth(authOptions)
+
+callbacks: {
+  async jwt({ token, user }) {
+    if (user) token.user = user
+    return token
+  },
+  async session({ session, token }) {
+    session.user = token.user
+    return session
+  },
+},
+
