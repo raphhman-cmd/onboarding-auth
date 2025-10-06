221723eeb62ca3c0b9ef954a1c90ce4b7f920295
raphhman-cmd <raph.hman@gmail.com>
2025-10-06 15:34:37 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://explainsmart.at" }, // deine Framer-Domain
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type,Authorization" },
+        ],
+      },
+    ]
+  },
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true
