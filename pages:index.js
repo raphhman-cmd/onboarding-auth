ec87aca31d41eb107ad08327e790efd71af2bebb
raphhman-cmd <raph.hman@gmail.com>
2025-10-06 11:53:24 +0200

    Delete pages:index.js

--- pages:index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "4rem", fontFamily: "sans-serif" }}>
-      <h1>Auth.js Server läuft 🚀</h1>
-      <p>Besuche <code>/api/auth/signin</code> um dich anzumelden.</p>
-    </div>
-  )
-}
